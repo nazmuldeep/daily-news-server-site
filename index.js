ee2731861cf86a42ec855a71b5a114ffe8a9a400
nazmuldip <nazmuldip21@gmail.com>
2022-10-22 19:56:44 +0600

    news json created

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 5000
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
 app.use(cors());
 app.get('/', (req, res) => {
     res.send('News Api')
 });
 app.get('/news-caregories', (req, res) => {
     res.send(categories)
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const category_news = news.filter(n = n.category_id === id)
+        res.send(category_news)
+    }
+})
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectNews = news.find(n => n._id == id);
+    res.send(selectNews)
 })
 
 app.listen(port, () => {
